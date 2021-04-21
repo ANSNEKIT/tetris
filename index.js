0c20e276c890fa6518255c0f02742c61ac35f9a7
ansnekit <ansnekit@gmail.com>
2021-04-22 01:53:33 +0300

    add view class and refactor

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const $root = document.getElementById('root');
+const options = {
+  width: 320,
+  height: 640,
+  rows: 20,
+  columns: 10,
+}
 
 const game = new Game();
+const view = new View($root, options);
 
 window.game = game;
+window.view = view;
 
-console.log(game);
+
+document.addEventListener('keydown', (evt) => {
+  switch (evt.key) {
+    case 'ArrowDown': 
+      game.movePieceDown();
+      view.render(game.getState());
+      break;
+    case 'ArrowLeft':
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    case 'ArrowRight':
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    case 'ArrowUp':
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+  }
+})
